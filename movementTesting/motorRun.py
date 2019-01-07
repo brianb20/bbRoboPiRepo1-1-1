0738cdb23fe6f2bbf4f424c6c919d808543921ae
brianb20 <bbianchi20@bsmschool.org>
2019-01-07 14:27:28 -0600

    Making sure every last change has been updated. I will be continuing to use this for a different bracket of programs.

--- movementTesting/motorRun.py
+++ movementTesting/motorRun.py
@@ -3,8 +3,15 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 import sys
 import time
 
+RPL.servoWrite(0, 2000)
+RPL.servoWrite(1, 1000)
+
+print("Snoozing...")
+time.sleep(30)
+print("Ok we done")
+
 RPL.servoWrite(0, 1500)
 RPL.servoWrite(1, 1500)
-
 RPL.servoWrite(0, 0)
 RPL.servoWrite(1, 0)
+
